c5e6b2f2aa255b88c58303cbe053b60d75caeebd
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2014-03-28 21:13:38 -0400

    Extend test to Distance

--- astropy/utils/numpy/tests/test_masked_quantity.py
+++ astropy/utils/numpy/tests/test_masked_quantity.py
@@ -5,20 +5,32 @@ from ..ma import MaskedArray
 
 from ....tests.helper import pytest
 from ....units import Quantity
+from ....coordinates import Longitude, Distance
 from .... import units as u
 
 
-class TestMaskedArrayWithQuantity():
+@pytest.fixture(scope='module', params=[Quantity, Distance])
+def quantity_type(request):
+    return request.param
+
+
+@pytest.mark.usefixtures('quantity_type')
+class TestMaskedArrayWithQuantity(object):
     """Test one can construct a masked array from a Quantity"""
 
-    def setup(self):
-        self.q = Quantity(np.arange(3., 6.), u.m)
-        self.mq = MaskedArray(self.q, mask=[True, False, False])
+    def _setup(self, quantity_type):
+        self.quantity_type = quantity_type
+        unit = {Quantity: u.m, Longitude: u.deg, Distance: u.kpc}
+        self.q = quantity_type(np.arange(3., 6.), unit[quantity_type])
+        self.mq = MaskedArray(self.q.copy(), mask=[True, False, False])
 
-    def test_baseclass(self):
+    def test_baseclass(self, quantity_type):
+        self._setup(quantity_type)
+        assert self.mq._baseclass is type(self.q)
         assert np.all(self.mq.data == self.q)
 
-    def test_access(self):
+    def test_access(self, quantity_type):
+        self._setup(quantity_type)
         mq_sel0 = self.mq[0]
         assert mq_sel0 is masked
         mq_sel1 = self.mq[1]
@@ -31,20 +43,23 @@ class TestMaskedArrayWithQuantity():
         assert np.all(mq_sel2.data == q_sel2)
         assert np.all(mq_sel2.mask == mask_sel2)
 
-    def test_setter(self):
-        q = np.arange(4., 7.) * u.m
-        mq = MaskedArray(np.arange(4., 7.) * u.m, mask=[False, False, False])
-        mq[1] = 1. * u.km
-        assert mq.data[1] == 1. * u.km
-        assert np.all((mq.data == q) == np.array([True, False, True]))
+    def test_setter(self, quantity_type):
+        self._setup(quantity_type)
+        self.mq[1] = 120. * self.q.unit
+        assert self.mq.data[1] == 120. * self.q.unit
+        assert np.all((self.mq.data == self.q) ==
+                      np.array([True, False, True]))
 
-    def test_filled(self):
+    def test_filled(self, quantity_type):
+        self._setup(quantity_type)
         assert np.all(self.mq.filled() == self.q.unit *
                       np.where(self.mq.mask, self.mq.fill_value, self.q.value))
 
-    def test_representation(self):
-        assert str(self.mq) == '[-- 4.0 5.0] m'
+    def test_representation(self, quantity_type):
+        self._setup(quantity_type)
+        assert str(self.mq) == '[-- 4.0 5.0] {0}'.format(self.q.unit)
         assert repr(self.mq) == (
-            'masked_Quantity(data = [-- 4.0 5.0] m,\n'
+            'masked_{0}(data = [-- 4.0 5.0] {1},\n'
             '                mask = [ True False False],\n'
-            '          fill_value = 1e+20)\n')
+            '          fill_value = 1e+20)\n'
+            .format(self.quantity_type.__name__, self.q.unit))
